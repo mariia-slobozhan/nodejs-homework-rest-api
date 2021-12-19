e3f9e4bc711ab333f5d639d3bbc7a97872d4312a
mariia-slobozhan <slobozhan.mi@gmail.com>
2021-12-19 12:05:40 +0200

    add logic

--- model/index.js
+++ model/index.js
@@ -1,20 +1,48 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
+const fs = require('fs/promises');
+const path = require('path');
+const crypto = require('crypto');
+const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+ const contactsPath = path.join(__dirname, 'contacts.json');
 
-const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
-
-const updateContact = async (contactId, body) => {}
-
-module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+const listContacts = async() => {
+  return contacts;
+  }
+  
+const getContactById = async (contactId) => {
+  const [result] = contacts.filter((contact) => contact.id === contactId);
+  return result;
+  }
+  
+const removeContact = async (contactId) => {
+  const deletedContact = contacts.find((contact) => contact.id === contactId);
+  const indexOfDeletedElement = contacts.indexOf(deletedContact);
+  const newList = contacts.splice(indexOfDeletedElement, 1)
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newList;
+    
+  }
+  
+const addContact = async ({ name, email, phone }) => {
+  const newContact = { id: crypto.randomUUID(), name, email, phone };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
+  
+const updateContact = async (contactId, body) => {
+  const updatedContact = contacts.find((contact) => contact.id === contactId);
+  const index = contacts.indexOf(updatedContact);
+  
+  if (index !== -1) {
+    const updatedContact = { id: contactId,...contacts[index], ...body };
+    contacts[index] = updatedContact;
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return updatedContact;
+  } 
+    
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact, updateContact };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,43 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const model = require('../../model/index');
+const validation = require('./validation')
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  const contacts = await model.listContacts();
+  res.status(200).json(contacts);
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.getContactById(id);
+  if (contact) {
+    return res.status(200).json(contact);    
+  }
+  res.status(404).json({ "message": 'Not found' });  
 })
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.post('/', validation.createValidation, async (req, res, next) => {
+  const contact = await model.addContact(req.body);
+  return res.status(201).json(contact);
 })
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.delete('/:id', async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await model.removeContact(id);
+    if (contact) {
+    return res.status(200).json({"message": "Contact deleted"});    
+  }
+  res.status(404).json({ "message": 'Not found' }); 
 })
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.put('/:id', validation.updateValidation, async (req, res, next) => {
+   const { id } = req.params;
+   const contact = await model.updateContact(id, req.body);
+   if (contact) {
+    return res.status(200).json(contact);    
+  }
+  res.status(404).json({"message": "Not found" });
 })
 
-module.exports = router
+module.exports = router;
